822f660cb222f90eb5d59b7ac24280ea9105ef8c
BobaBinks <marcusmc78@gmail.com>
2026-01-18 05:40:12 -0500

    Exercise 3 WIP

--- service/routes.py
+++ service/routes.py
@@ -106,9 +106,18 @@ def create_products():
 # R E A D   A   P R O D U C T
 ######################################################################
 
-#
-# PLACE YOUR CODE HERE TO READ A PRODUCT
-#
+@app.route("/products/<int:product_id>", methods=["GET"])
+def read_product(product_id):
+    """
+    Read a Product
+    This endpoint will Read a Product based the data in the body that is posted
+    """
+    product = Product.find(product_id)
+
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
+
+    return product.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # U P D A T E   A   P R O D U C T
